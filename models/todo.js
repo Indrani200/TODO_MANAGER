3989fb5894d7941d487e0ac8ffb36a9ede7e7549
Indrani200 <113599635+Indrani200@users.noreply.github.com>
2023-01-04 22:17:50 +0530

    Add files via upload

--- models/todo.js
+++ models/todo.js
@@ -1,21 +1,27 @@
 "use strict";
-const { Model,Op } = require("sequelize");
+const { Model, Op } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Todo extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
     static associate(models) {
       // define association here
     }
-    static getTodos(){
-      return this.findAll();
-    }
-    
-    static addTodo({ title, dueDate}) {
+
+    static addTodo({ title, dueDate }) {
       return this.create({ title: title, dueDate: dueDate, completed: false });
     }
 
-    markAsCompleted() {
-      return this.update({ completed: true });
+    static getTodos() {
+      return this.findAll();
     }
+
+    // markAsCompleted() {
+    //   return this.update({ completed: true });
+    // }
     static async overdue() {
       return await Todo.findAll({
         where: {
@@ -25,7 +31,7 @@ module.exports = (sequelize, DataTypes) => {
         order: [["id", "ASC"]],
       });
     }
-    static async dueToday(){
+    static async dueToday() {
       return await Todo.findAll({
         where: {
           dueDate: { [Op.eq]: new Date().toLocaleDateString("en-CA") },
@@ -43,6 +49,22 @@ module.exports = (sequelize, DataTypes) => {
         order: [["id", "ASC"]],
       });
     }
+
+    static async completedTodos() {
+      return await Todo.findAll({
+        where: { completed: true },
+      });
+    }
+    static async remove(id) {
+      return this.destroy({
+        where: {
+          id,
+        },
+      });
+    }
+    setCompletionStatus(completed) {
+      return this.update({ completed: completed });
+    }
   }
   Todo.init(
     {
@@ -57,3 +79,4 @@ module.exports = (sequelize, DataTypes) => {
   );
   return Todo;
 };
+
